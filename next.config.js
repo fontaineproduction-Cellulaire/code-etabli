35fed9e98cd444e097dcb975d51c97c022b21338
fontaineproduction-Cellulaire <fontaineproduction@gmail.com>
2026-05-30 15:47:00 -0400

    Augmenter limite body parser pour gros PDFs (50mb)

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-          serverComponentsExternalPackages: ['pdf-parse', '@prisma/client'],
-    },
-    webpack: (config) => {
-          config.resolve.alias.canvas = false
-          return config
-    },
+      experimental: {
+              serverComponentsExternalPackages: ['pdf-parse', '@prisma/client'],
+      },
+      webpack: (config) => {
+              config.resolve.alias.canvas = false
+              return config
+      },
+      // Augmenter la limite de taille pour les uploads de gros PDFs (ex: CCQ 14.8Mo)
+      serverRuntimeConfig: {
+              bodyParser: {
+                        sizeLimit: '50mb',
+              },
+      },
 }
 
 module.exports = nextConfig
